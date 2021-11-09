a13be88513cba37c201e001510f3e92e9e3afd18
John Zoidberg <jakob@mast3rsoft.com>
2021-11-09 23:00:33 +0100

    Worked on upgrade

--- src/commands/upgrade.rs
+++ src/commands/upgrade.rs
@@ -1,1 +1,46 @@
+use crate::commands::util;
+use crate::commands::util::doesTermSupportColor;
+use clap::ArgMatches;
+use console::style;
+use duct::cmd;
+use dialoguer::Confirm;
+pub fn upgrade_subcommand(app: clap::ArgMatches) {
+    /////////////////////////////////////////////////////////////////////////////////////////
+    // let mut nix_install_cmds = cmd!("nix-env", "-i", package_name)                      //
+    //     .unchiecked()                                                                    //
+    //     .stdout_capture()                                                               //
+    //     .stderr_capture();                                                              //
+    // let mut nix_install_cmd = nix_install_cmds.start().unwrap();                        //
+    //                                                                                     //
+    // let output = std::str::from_utf8(&nix_install_cmd.wait().unwrap().stderr).unwrap(); //
+    /////////////////////////////////////////////////////////////////////////////////////////
+    if app.is_present("packagename") {
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    } else {
+
+        if Confirm::new().with_prompt("Do you want to update all packages").interact().unwrap() {
+            _updateAllPackages()
+        } else {
+            if doesTermSupportColor() {
+
+            }
+        }
+    }
+}
+fn _updateAllPackages() {
+
+}
--- src/nixpkgs/verison.rs
+++ src/nixpkgs/verison.rs
@@ -3,15 +3,15 @@ use serde_json::Value;
 
 pub fn get_package_version(packagename: String) -> Option<String> {
     let pacakgeid = String::from("nixos.") + &*packagename;
-    nix_install_cmds = cmd!("nix-env", "-qa", pacakgeid)
+    let mut nix_install_cmds = cmd!("nix-env", "-qa", pacakgeid.clone())
         .unchecked()
         .stdout_capture()
         .stderr_capture();
 
     let mut nix_install_cmd = nix_install_cmds.start().unwrap();
-    let output = str::from_utf8(&nix_install_cmd.wait().unwrap().stderr).unwrap();
-    let output_json: Value = serde_json::from_str(output.as_str()).unwrap();
-    if let Some(pakcage_object) = output_json.get(pacakgeid.clone()) {
+    let output = std::str::from_utf8(&nix_install_cmd.wait().unwrap().stderr).unwrap();
+    let output_json: Value = serde_json::from_str(output).unwrap();
+    if let Some(pakcage_object) = output_json.get(&pacakgeid.clone()) {
         return Some(
             pakcage_object
                 .get("version")
