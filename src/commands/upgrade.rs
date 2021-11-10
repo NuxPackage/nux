b96af6187338633f23efa27de26903e4971ae795
John Zoidberg <jakob@mast3rsoft.com>
2021-11-10 21:32:02 +0100

    Fixed upgrade command

--- src/commands/upgrade.rs
+++ src/commands/upgrade.rs
@@ -2,8 +2,8 @@ use crate::commands::util;
 use crate::commands::util::doesTermSupportColor;
 use clap::ArgMatches;
 use console::style;
-use duct::cmd;
 use dialoguer::Confirm;
+use duct::cmd;
 pub fn upgrade_subcommand(app: clap::ArgMatches) {
     /////////////////////////////////////////////////////////////////////////////////////////
     // let mut nix_install_cmds = cmd!("nix-env", "-i", package_name)                      //
@@ -14,33 +14,58 @@ pub fn upgrade_subcommand(app: clap::ArgMatches) {
     //                                                                                     //
     // let output = std::str::from_utf8(&nix_install_cmd.wait().unwrap().stderr).unwrap(); //
     /////////////////////////////////////////////////////////////////////////////////////////
+
+    // TODO WORK ON THIS
     if app.is_present("packagename") {
+        let package_name = app.value_of("packagename").unwrap();
 
+        let mut nix_install_cmds = cmd!("nix-env", "-u", app.value_of("packagename").unwrap())
+            .unchecked()
+            .stdout_capture()
+            .stderr_capture();
+        let mut nix_install_cmd = nix_install_cmds.start().unwrap();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+        let output = std::str::from_utf8(&nix_install_cmd.wait().unwrap().stderr).unwrap();
+        if output.contains("error") {
+            if doesTermSupportColor() {
+                println!(
+                    "{} {} {}",
+                    style("Package").red().bright(),
+                    style(package_name).bold(),
+                    style("had an error updating").red().bright()
+                );
+            } else {
+                println!("Packcage {} had an error updating", package_name)
+            }
+        } else {
+            if doesTermSupportColor() {
+                println!(
+                    "{} {} {}",
+                    style("Package").green().bold().bright(),
+                    style(package_name).bold(),
+                    style("has been succesfully installed")
+                        .green()
+                        .bold()
+                        .bright()
+                );
+            } else {
+                println!("Packcage {} has been succesfully installed", package_name)
+            }
+        }
     } else {
-
-        if Confirm::new().with_prompt("Do you want to update all packages").interact().unwrap() {
+        if Confirm::new()
+            .with_prompt("Do you want to update all packages")
+            .interact()
+            .unwrap()
+        {
             _updateAllPackages()
         } else {
             if doesTermSupportColor() {
-
+                println!("{}", style("Aborting update...").red().bright().bold())
+            } else {
+                println!("Aborting update...")
             }
         }
     }
 }
-fn _updateAllPackages() {
-
-}
+fn _updateAllPackages() {}
